bb3d7bcfb57a7f0727d277f4415093e4dc042516
Joey Hoer <me@joeyhoer.com>
2017-02-23 17:38:56 -0500

    Adding apngdis

---
+++ apngdis.rb
@@ -0,0 +1,11 @@
+class Apngdis < Formula
+  desc "Deconstruct APNG file into a sequence of PNG frames"
+  homepage "http://apngdis.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/apngdis/2.8/apngdis-2.8-bin-macosx.zip"
+  version "2.8"
+  sha256 "2f5a80e241690522aedcda06f01e93eebac5f4baef9b472efe8ed4343e59408c"
+
+  def install
+    bin.install "apngdis"
+  end
+end
