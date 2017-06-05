3115cec11c0dd2d924e08063a958a196cca88878
Joey Hoer <me@joeyhoer.com>
2017-06-05 18:33:41 -0400

    Adding nice-screenshot package

---
+++ nice-screenshot.rb
@@ -0,0 +1,11 @@
+class NiceScreenshot < Formula
+  desc "Intelligently resize, trim and frame images"
+  homepage "https://github.com/joeyhoer/nice-screenshot"
+  head "https://github.com/joeyhoer/nice-screenshot.git"
+
+  depends_on "imagemagick"
+
+  def install
+    bin.install "nice-screenshot.sh" => "nice-screenshot"
+  end
+end
