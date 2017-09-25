1ff54a72584b821796e681747fcddbefe0358d87
Joey Hoer <me@joeyhoer.com>
2017-09-25 14:43:36 -0400

    Updating mediancut-posterizer

--- mediancut-posterizer.rb
+++ mediancut-posterizer.rb
@@ -2,11 +2,17 @@ class MediancutPosterizer < Formula
   desc "Reduces number of distinct color/alpha intensities in the image."
   homepage "https://github.com/pornel/mediancut-posterizer"
   version "1.6"
+  head "https://github.com/joeyhoer/gifv.git"
   # https://github.com/pornel/mediancut-posterizer/archive/1.6.tar.gz
   url "https://github.com/pornel/mediancut-posterizer.git", :using => :git
 
+  option "with-cocoa", "Build with COCOA PNG reader"
+
   def install
-    system "make"
+    args = []
+    args << "USE_COCOA=1" if build.with? "cocoa"
+
+    system "make", *args
     bin.install "posterize"
   end
 
