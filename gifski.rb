797c92fbafa0f4cf3df71cb32737c38d84d1eb25
Joey Hoer <me@joeyhoer.com>
2017-11-02 09:14:29 -0400

    Adding gifski

---
+++ gifski.rb
@@ -0,0 +1,11 @@
+class Gifski < Formula
+  desc "GIF encoder based on libimagequant (pngquant, gifquant?). Squeezes maximum possible quality from the awful GIF format."
+  homepage "https://github.com/ImageOptim/gifski"
+  url "https://github.com/ImageOptim/gifski/releases/download/0.1.0/gifski-0.1.0.zip"
+  version "0.1.0"
+  sha256 "82cfecaabb5baee956f252670fc22dfcf1807077d3b00beb31c0b83feac2c1ec"
+
+  def install
+    bin.install "mac/gifski" => "gifski"
+  end
+end
