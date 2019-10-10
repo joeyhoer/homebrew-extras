86042f008015dd6b5475d7dc36c7d9a2b3f744f0
Yurii Kolesnykov <root@yurikoles.com>
2019-10-10 23:50:46 +0300

    Fixes for font-microsoft-office
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/font-microsoft-office.rb
+++ Casks/font-microsoft-office.rb
@@ -1,38 +1,41 @@
 cask 'font-microsoft-office' do
-  version :latest
+  version '20170623'
   sha256 '249473568eba7a1e4f95498acba594e0f42e6581add4dead70c1dfb908a09423'
 
-  url 'https://fossies.org/windows/misc/PowerPointViewer.exe'
+  # business.uwm.edu/gdrive/Dietenberger_E/PowerPointViewer.exe was verified as official when first introduced to the cask
+  url 'http://www.business.uwm.edu/gdrive/Dietenberger_E/PowerPointViewer.exe'
+  name 'PowerPoint Fonts'
+  homepage 'https://www.office.com/'
 
-  depends_on :formula => 'cabextract'
-  container :nested => 'ppviewer.cab'
+  depends_on formula: 'cabextract'
+  container nested: 'ppviewer.cab'
 
-  font 'CALIBRI.TTF',  :target => 'Calibri.ttf'
-  font 'CALIBRIB.TTF', :target => 'CalibriBold.ttf'
-  font 'CALIBRII.TTF', :target => 'CalibriItalic.ttf'
-  font 'CALIBRIZ.TTF', :target => 'CalibriBoldItalic.ttf'
-  font 'CAMBRIA.TTC',  :target => 'Cambria.ttc'
-  font 'CAMBRIAB.TTF', :target => 'CambriaBold.ttf'
-  font 'CAMBRIAI.TTF', :target => 'CambriaItalic.ttf'
-  font 'CAMBRIAZ.TTF', :target => 'CambriaBoldItalic.ttf'
-  font 'CANDARA.TTF',  :target => 'Candara.ttf'
-  font 'CANDARAB.TTF', :target => 'CandaraBold.ttf'
-  font 'CANDARAI.TTF', :target => 'CandaraItalic.ttf'
-  font 'CANDARAZ.TTF', :target => 'CandaraBoldItalic.ttf'
-  font 'CONSOLA.TTF',  :target => 'Consola.ttf'
-  font 'CONSOLAB.TTF', :target => 'ConsolaBold.ttf'
-  font 'CONSOLAI.TTF', :target => 'ConsolaItalic.ttf'
-  font 'CONSOLAZ.TTF', :target => 'ConsolaBoldItalic.ttf'
-  font 'CONSTAN.TTF',  :target => 'Constan.ttf'
-  font 'CONSTANB.TTF', :target => 'ConstanBold.ttf'
-  font 'CONSTANI.TTF', :target => 'ConstanItalic.ttf'
-  font 'CONSTANZ.TTF', :target => 'ConstanBoldItalic.ttf'
-  font 'CORBEL.TTF',   :target => 'Corbel.ttf'
-  font 'CORBELB.TTF',  :target => 'CorbelBold.ttf'
-  font 'CORBELI.TTF',  :target => 'CorbelItalic.ttf'
-  font 'CORBELZ.TTF',  :target => 'CorbelBoldItalic.ttf'
-  font 'MEIRYO.TTC',   :target => 'Meiryo.ttc'
-  font 'MEIRYOB.TTC',  :target => 'MeiryoBold.ttc'
+  font 'CALIBRI.TTF',  target: 'Calibri.ttf'
+  font 'CALIBRIB.TTF', target: 'CalibriBold.ttf'
+  font 'CALIBRII.TTF', target: 'CalibriItalic.ttf'
+  font 'CALIBRIZ.TTF', target: 'CalibriBoldItalic.ttf'
+  font 'CAMBRIA.TTC',  target: 'Cambria.ttc'
+  font 'CAMBRIAB.TTF', target: 'CambriaBold.ttf'
+  font 'CAMBRIAI.TTF', target: 'CambriaItalic.ttf'
+  font 'CAMBRIAZ.TTF', target: 'CambriaBoldItalic.ttf'
+  font 'CANDARA.TTF',  target: 'Candara.ttf'
+  font 'CANDARAB.TTF', target: 'CandaraBold.ttf'
+  font 'CANDARAI.TTF', target: 'CandaraItalic.ttf'
+  font 'CANDARAZ.TTF', target: 'CandaraBoldItalic.ttf'
+  font 'CONSOLA.TTF',  target: 'Consola.ttf'
+  font 'CONSOLAB.TTF', target: 'ConsolaBold.ttf'
+  font 'CONSOLAI.TTF', target: 'ConsolaItalic.ttf'
+  font 'CONSOLAZ.TTF', target: 'ConsolaBoldItalic.ttf'
+  font 'CONSTAN.TTF',  target: 'Constan.ttf'
+  font 'CONSTANB.TTF', target: 'ConstanBold.ttf'
+  font 'CONSTANI.TTF', target: 'ConstanItalic.ttf'
+  font 'CONSTANZ.TTF', target: 'ConstanBoldItalic.ttf'
+  font 'CORBEL.TTF',   target: 'Corbel.ttf'
+  font 'CORBELB.TTF',  target: 'CorbelBold.ttf'
+  font 'CORBELI.TTF',  target: 'CorbelItalic.ttf'
+  font 'CORBELZ.TTF',  target: 'CorbelBoldItalic.ttf'
+  font 'MEIRYO.TTC',   target: 'Meiryo.ttc'
+  font 'MEIRYOB.TTC',  target: 'MeiryoBold.ttc'
 
   caveats <<~EOS
     PowerPoint Viewer was retired on April 30, 2018. It's no
@@ -44,5 +47,5 @@ cask 'font-microsoft-office' do
     You may use the fonts that accompany the PowerPoint Viewer
     only to display and print content from a device running a
     Microsoft Windows operating system.
-    EOS
+  EOS
 end
