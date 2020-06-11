98924d5c1cade04642b6efbb033dae2c9c0a34dc
Joey Hoer <me@joeyhoer.com>
2020-06-11 12:56:07 -0400

    Adding placeholder formula

---
+++ placeholder.rb
@@ -0,0 +1,15 @@
+class Placeholder < Formula
+  desc "Create placeholder images easily from the command line"
+  homepage "https://github.com/joeyhoer/placeholder"
+  head "https://github.com/joeyhoer/placeholder.git"
+
+  depends_on "ffmpeg"
+
+  def install
+    bin.install "placeholder.sh" => "placeholder"
+  end
+
+  test do
+    system "#{bin}/placeholder", "100x100.png"
+  end
+end
